9e804254be85a73a7dcc881092a1afbbf5e5ceb6
arjunaaditiyaa <arjunaaditiyaa2005@gmail.com>
2025-12-23 11:26:03 +0530

    vacination schedule sql model

---
+++ main.py
@@ -0,0 +1,13 @@
+import json
+import requests
+from typing import Optional, List, Type
+from datetime import datetime
+from sqlmodel import SQLModel, Field, create_engine, Session, select
+from openai import OpenAI
+
+
+class VaccinationSchedule(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    target_disease: str
+    age_group: str
+    schedule_details: str
