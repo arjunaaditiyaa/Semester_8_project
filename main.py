f30ee544605841683602a877b0f144b0da5d5889
arjunaaditiyaa <arjunaaditiyaa2005@gmail.com>
2025-12-23 11:32:45 +0530

    added sql model initializzation

--- main.py
+++ main.py
@@ -27,3 +27,23 @@ class DiseaseOutbreak(SQLModel, table=True):
 
 
 engine = create_engine("sqlite:///healthcare_expert.db")
+def init_db():
+
+    SQLModel.metadata.create_all(engine)
+    with Session(engine) as session:
+
+        if not session.exec(select(VaccinationSchedule)).first():
+            session.add(VaccinationSchedule(target_disease="Polio", age_group="Infants",
+                                            schedule_details="4 doses at 2, 4, 6-18 months, and 4-6 years."))
+            session.add(VaccinationSchedule(target_disease="Hepatitis B", age_group="Infants",
+                                            schedule_details="3 doses: Birth, 1-2 months, 6-18 months."))
+
+
+        if not session.exec(select(SymptomGuide)).first():
+            session.add(
+                SymptomGuide(disease_name="Cholera", common_symptoms="Severe watery diarrhea, vomiting, leg cramps.",
+                             prevention="Safe water, handwashing, vaccine."))
+            session.add(
+                SymptomGuide(disease_name="Mpox", common_symptoms="Rash with blisters, fever, swollen lymph nodes.",
+                             prevention="Avoid skin contact with infected, vaccination."))
+        session.commit()
